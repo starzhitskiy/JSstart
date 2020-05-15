0dfbf661f5f00c153193922311653c1252b53702
starzhitskiy <study.html.css.java@gmail.com>
2020-05-15 19:03:49 +0300

    startJS

---
+++ app.js
@@ -0,0 +1,45 @@
+'use strict'
+// let name = prompt('Какое «официальное» название JavaScript?', '');
+//  if (name == 'ECMAScript' || ('ecmascript')) {
+// 	 alert('Правильно!');
+//  } else{
+// 	alert('Не знаете? “ECMAScript”!');
+//  }
+
+// let namber = prompt('Введите число?', '');
+//  if (namber > 0) {
+// 	alert = ('1');
+//  } else if  (namber < 0) {
+// 	alert = ('-1');
+//  } else {
+// 	alert = ('0');
+// } 
+
+let text = prompt ('Who\'s there?');
+ if (text == 'Отмена') {
+	 alert ('Отменено');
+ } else if (text == 'Другое') {
+	 alert ('I don\'t know you');
+ } else if (text =='Админ')  {
+	 
+	let pass = prompt ('Пароль?');
+		if (pass == 'Отмена') {
+			alert ('Отмнено');
+		}
+		else {
+			alert ('Неверный пароль');
+		}if (pass == 'Я Главный') {
+			alert ('Здравствуйте!');
+		}
+ }
+
+
+
+
+
+
+
+
+
+
+
